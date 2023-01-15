60bc4f7149a5490576a8f47dc1a56e34f9fa2442
ThalesFerreira <thalesferreira190@gmail.com>
2023-01-15 07:01:26 -0300

    inclui Projeto ajustes finais1

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -16,7 +16,7 @@ const options = {
   database: 
     `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  password: process.env.MYSQL_PASSWORD || 'senhaDoDB',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
